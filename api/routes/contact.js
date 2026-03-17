c0f589fa0b19d569a5ef734c1dde4d11625bb30a
Tiffany Yu <tif03.yu@gmail.com>
2026-03-17 18:49:13 -0400

    redo contact email connection

--- api/routes/contact.js
+++ api/routes/contact.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const db = require('../db')
+const { Resend } = require('resend')
+
+const resent = new resent(process.env.RESEND_API_KEY)
 
 router.post('/', async (req, res) => {
   const { name, email, message } = req.body
@@ -16,6 +19,13 @@ router.post('/', async (req, res) => {
 
   const insert = db.prepare('INSERT INTO messages (name, email, message) VALUES (?, ?, ?)')
   insert.run(name, email, message)
+
+  await Resend.emails.send({
+    from: 'onboarding@resend.dev',
+    to: 'ty427@cornell.edu',
+    subject : `New Message from ${name}`,
+    text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`
+  })
 
   res.json({ message: 'Message received!' })
 })
